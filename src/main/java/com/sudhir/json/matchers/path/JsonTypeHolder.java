feace735b70b5a924d259b3bec103638e81ab23c
sudhirbits@gmail.com <sudhirbits@gmail.com@288bba9e-23b2-c904-c46b-458c89193b30>
2011-06-30 21:07:42 +0000

    Sudhir: - javadoc added.

--- src/main/java/com/sudhir/json/matchers/path/JsonTypeHolder.java
+++ src/main/java/com/sudhir/json/matchers/path/JsonTypeHolder.java
@@ -1,8 +1,16 @@
-/**
- * 
- */
 package com.sudhir.json.matchers.path;
 
+/**
+ * Holds JSONObject/JSONArray and provides the held instance to the get call. 
+ * 
+ * org.json.JSONObject and JSONArray do not extend a common interface/class. 
+ * Hence the APIs to deal with both together have to deal with raw Objects. 
+ * JsonTypeHolder is a slightly better abstraction to give some type checks. 
+ * @author Sudhir
+ * @since 1.0
+ *
+ * @param <W> org.json.JSONObject/org.json.JSONArray
+ */
 public class JsonTypeHolder<W> {
 	W jsonTypeInstance;
 	public JsonTypeHolder(W jsonTypeInstance) {
